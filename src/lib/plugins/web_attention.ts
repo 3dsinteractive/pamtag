80329853c2e142bd9f47ab59eb268208a988bee0
Heart Narongrit <narongrit@3dsinteractive.com>
2023-05-15 11:58:59 +0700

    fixed error

---
+++ src/lib/plugins/web_attention.ts
@@ -0,0 +1,8 @@
+import PamTracker from "..";
+import { Plugin } from "../core/plugin";
+
+export class WebSttenTionPlugin extends Plugin {
+  // override initPlugin(pam: PamTracker): void {
+  //   pam.hook.onStartup();
+  // }
+}
