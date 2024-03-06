347f34d052ad9a7a882a427fd725c9125bdba673
Heart Narongrit <narongrit@3dsinteractive.com>
2024-03-06 10:00:49 +0700

    add publish script

---
+++ publish.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "Select the version number increment type."
+echo "Please choose an option:"
+echo "1. patch"
+echo "2. minor"
+echo "3. major"
+echo "Other. Do not increment"
+
+read -p "Enter your choice (1, 2, or 3): " user_choice
+
+case $user_choice in
+  1)
+    npm version patch
+    ;;
+  2)
+    npm version minor
+    ;;
+  3)
+    npm version major
+    ;;
+  *)
+    echo "No increment"
+    ;;
+esac
+
+
+
+
+
